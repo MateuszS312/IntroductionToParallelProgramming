ae46de672470b3e4cb5efe1e44d7d7f768ec5127
Mateusz Samsel <matek3122@gmail.com>
2021-12-05 15:28:09 +0100

    Thread Pools!
    Multithreading by pools added

--- wdprir01/src/com/company/Main.java
+++ wdprir01/src/com/company/Main.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.lang.Math;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main
@@ -55,6 +57,68 @@ public class Main
             {
                 for(int yy=0;yy<height; yy++)
                 {
+                    double aa=range_re[0]+xx*(range_re[1]-range_re[0])/(width-1);
+                    double bb=range_im[0]+yy*(range_im[1]-range_im[0])/(height-1);
+                    if(check_condition(aa,bb,max_itr))
+                        image.setRGB(xx, yy, Color.BLACK.getRGB());
+                    else
+                        image.setRGB(xx, yy, Color.WHITE.getRGB());
+
+                }
+            }
+//            System.out.println("Thread ended:"+Thread.currentThread().getId());
+        }
+    }
+    public static class FPpools implements Runnable
+    {
+        BufferedImage image;
+        int width;
+        int height;
+        double[] range_re;
+        double[] range_im;
+        int max_itr;
+        int block_dimx;
+        int block_dimy;
+        int thread_idx;
+        int thread_idy;
+        FPpools(BufferedImage im, int wd, int hg,double[] rng_re,double[] rng_im, int max_iterations, int blck_dimx,int blck_dimy, int thrd_idx,int thrd_idy)
+        {
+            image=im;
+            width=wd;
+            height=hg;
+            range_re=rng_re;
+            range_im=rng_im;
+            max_itr=max_iterations;
+            block_dimx=blck_dimx;
+            block_dimy=blck_dimy;
+            thread_idx=thrd_idx;
+            thread_idx=thrd_idy;
+        }
+        FPpools(BufferedImage im, int wd, int hg, int blck_dimx,int blck_dimy, int thrd_idx, int thrd_idy)
+        {
+            image=im;
+            width=wd;
+            height=hg;
+            range_re=new double[] {-2.1,0.6};
+            range_im=new double[]  {-1.2,1.2};
+            max_itr=200;
+            block_dimx=blck_dimx;
+            block_dimy=blck_dimy;
+            thread_idx=thrd_idx;
+            thread_idy=thrd_idy;
+        }
+
+        @Override
+        public void run()
+        {
+//            System.out.println("Thread started:"+Thread.currentThread().getId());
+//            System.out.println("idx: "+thread_idx+"dim:"+block_dimx+"idy: "+thread_idy+"dim:"+block_dimy);
+
+            for(int xx=thread_idx*block_dimx;xx<(thread_idx+1)*block_dimx; xx++)
+            {
+                for(int yy=thread_idy*block_dimy;yy<(thread_idy+1)*block_dimy; yy++)
+                {
+//                    System.out.println(thread_idx*block_dimx+" "+thread_idy*block_dimy +" "+xx+" "+yy+"Thread started:"+Thread.currentThread().getId());
                     double aa=range_re[0]+xx*(range_re[1]-range_re[0])/(width-1);
                     double bb=range_im[0]+yy*(range_im[1]-range_im[0])/(height-1);
                     if(check_condition(aa,bb,max_itr))
@@ -194,6 +258,52 @@ public class Main
     }
 
 
+
+    public static double measure_time_pools(int width, int height, int nn, int num_of_blocksx,int num_of_blocksy)
+    {
+        double[] timings=new double[nn];
+        BufferedImage pixels=new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        for(int ii=0;ii<nn;ii++)
+        {
+            pixels = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            ExecutorService ex = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            long start_time=System.nanoTime();
+            for(int xx=0;xx<num_of_blocksx;xx++)
+            {
+                for(int yy=0;yy<num_of_blocksy;yy++)
+                {
+//                System.out.println(width/4);
+                    ex.execute(new FPpools(pixels, width, height,width/num_of_blocksx,height/num_of_blocksy,xx,yy));
+
+                }
+            }
+            ex.shutdown();
+            try {
+                ex.awaitTermination(1,TimeUnit.DAYS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            long finish_time=System.nanoTime();
+            timings[ii]=finish_time-start_time;
+
+
+        }
+        double avg_time=0;
+        for(var value:timings)
+        {
+            avg_time+=value;
+        }
+        avg_time/=nn;
+        save_picture(pixels,"pools");
+
+        return avg_time*Math.pow(10,-9);
+    }
+
+    public static double measure_time_pools(int width, int height, int nn)
+    {
+        return measure_time_pools(width, height, nn, 2, 2);
+    }
+
     public static void write_output(String filename, int[] dimensions, double[] time)
     {
         BufferedWriter outputWriter = null;
@@ -225,14 +335,14 @@ public class Main
         double[] time= new double[dimensions.length];
         int width=1920;
         int height=1920;
-
-//        for(int ii=0;ii<dimensions.length;ii++)
-//        {
-//            time[ii]=measure_time(dimensions[ii],dimensions[ii],200);
-//            System.out.println(time[ii]);
-//        }
-//        write_output("out.txt",dimensions,time);
-//        System.out.println(time.toString());
+        int[] window_size=new int[] {4,8,16, 32, 64, 128};
+        for(int ii=0;ii<dimensions.length;ii++)
+        {
+            time[ii]=measure_time(dimensions[ii],dimensions[ii],200);
+            System.out.println(time[ii]);
+        }
+        write_output("out.txt",dimensions,time);
+        System.out.println(time.toString());
 
         for(int ii=0;ii<dimensions.length;ii++)
         {
@@ -241,7 +351,26 @@ public class Main
         }
         write_output("out_threads.txt",dimensions,time);
         System.out.println(time.toString());
+        for(int ii=0;ii<dimensions.length;ii++)
+        {
+            time[ii]=measure_time_pools(dimensions[ii],dimensions[ii],200,2,2);
+            System.out.println(time[ii]);
 
+        }
+        write_output("out_pools"+2+".txt",dimensions,time);
+        System.out.println(time.toString());
+        for(var size:window_size)
+        {
+            for(int ii=0;ii<dimensions.length;ii++)
+            {
+                time[ii]=measure_time_pools(dimensions[ii],dimensions[ii],200,dimensions[ii]/size,dimensions[ii]/size);
+                System.out.println(time[ii]);
+                write_output("out_pools"+size+".txt",dimensions,time);
+            }
+
+        }
+
+        System.out.println("Ready");
 
     }
 
